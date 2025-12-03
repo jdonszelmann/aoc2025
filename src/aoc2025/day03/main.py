14c9fe93a56ebeb3a08dbce283889b99046cc976
Jana Dönszelmann <jana@donsz.nl>
2025-12-03 22:35:51 +0100

    day 3

---
+++ src/aoc2025/day03/main.py
@@ -0,0 +1,35 @@
+from functools import partial, reduce
+
+from ..shared import lines, run
+
+
+def argmax(line):
+    return max(range(len(line)), key=lambda x: line[x])
+
+
+def largest(i, n, r):
+    return [
+        reduce(
+            lambda e, d: [m := argmax(e[: len(e) - d + 1]), r.append(e[m]), e[m + 1 :]][
+                -1
+            ],
+            range(n, 0, -1),
+            i,
+        ),
+        int("".join(r)),
+    ][-1]
+
+
+def part1(lines):
+    return sum(largest(line, 2, []) for line in lines)
+
+
+def part2(lines):
+    return sum(largest(line, 12, []) for line in lines)
+
+
+def main():
+    run("test1", part1, lines)
+    run("part1", part1, lines)
+    run("test1", part2, lines)
+    run("part1", part2, lines)
